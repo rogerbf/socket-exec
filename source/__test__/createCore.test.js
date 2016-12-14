cc7217929c7a4a9d19df061a46738d07d452bf5c
rogerbf <roger@bergfalk.nu>
2016-12-15 00:00:15 +0100

    fix tests

--- source/__test__/createCore.test.js
+++ source/__test__/createCore.test.js
@@ -1,17 +1,23 @@
 import createCore from '../library/createCore.js'
 
-test(`createCore`, () => {
-  it(`exports a function`, () => {
-    expect(typeof (createCore)).toEqual(`function`)
-  })
+it(`exports a function`, () => {
+  expect(typeof (createCore)).toEqual(`function`)
+})
 
-  it(`it returns the expected output`, () => {
-    const greeter = (configuration) => ({ greet: () => configuration.hello })
-    const configuration = { hello: `hej` }
-    const dependencies = [ greeter ]
-    const core = createCore({ configuration, dependencies })
-    expect(typeof (core)).toEqual(`object`)
-    expect(core.hasOwnProperty(`greet`)).toBeTruthy()
-    expect(core.greet()).toEqual(`hej`)
+it(`returns the expected output`, () => {
+  expect(createCore()).toEqual({})
+})
+
+it(`it returns the expected output`, () => {
+  const greeter = (configuration) => ({ greet: () => configuration.hello })
+  const speaker = (configuration) => ({ speak: (phrase) => phrase })
+  const core = createCore({
+    configuration: { hello: `hej` },
+    dependencies: [ greeter, speaker ]
   })
+  expect(typeof (core)).toEqual(`object`)
+  expect(core.hasOwnProperty(`greet`)).toBeTruthy()
+  expect(core.greet()).toEqual(`hej`)
+  expect(core.hasOwnProperty(`speak`)).toBeTruthy()
+  expect(core.speak(`farewell`)).toEqual(`farewell`)
 })
--- source/__test__/index.test.js
+++ source/__test__/index.test.js
@@ -1,8 +1,7 @@
 import socketExec from '../index.js'
 
-test(`socketExec`, () => {
-  it(`is defined`, () => expect(socketExec).toBeDefined())
-  it(`returns a rmeote object`, () => {
-    expect(typeof (socketExec())).toEqual(`object`)
-  })
+it(`is defined`, () => expect(socketExec).toBeDefined())
+
+it(`returns a rmeote object`, () => {
+  expect(typeof (socketExec())).toEqual(`object`)
 })

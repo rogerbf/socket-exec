816bd5ea0024769d381afc4b17baabe1c70afe05
rogerbf <roger@bergfalk.nu>
2016-12-16 16:53:55 +0100

    wrap message in Error object

--- source/library/execute.js
+++ source/library/execute.js
@@ -11,7 +11,7 @@ export default (configuration, connect = createConnection) => ({
         instructions.map(instruction => socket.write(instruction))
       )
       socket.on(`error`, error => reject(error))
-      socket.on(`timeout`, () => socket.destroy(`timeout reached`))
+      socket.on(`timeout`, () => socket.destroy(Error(`Timeout`)))
 
       socket.pipe(concatStream)
     })
